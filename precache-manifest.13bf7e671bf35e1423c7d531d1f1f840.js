77f8bc199ec67b00bcd87986caed40fe0954d47a
Omid Omidvar <omid.omidvaar@gmail.com>
2024-10-08 10:13:45 +0100

    Updates

---
+++ precache-manifest.13bf7e671bf35e1423c7d531d1f1f840.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b5e4d81d5a15bd64142a28673887cd",
+    "url": "/Org-structure/index.html"
+  },
+  {
+    "revision": "16e294f10581a021b43d",
+    "url": "/Org-structure/static/js/main.f660448d.chunk.js"
+  },
+  {
+    "revision": "a9efd50a28d18196d325",
+    "url": "/Org-structure/static/js/runtime~main.ea8a93e3.js"
+  }
+]);
